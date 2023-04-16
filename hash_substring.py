94539943a9db384c38c62f679cae66d7eedf2242
Ņina Aļjanaki <125008313+221RDB018@users.noreply.github.com>
2023-04-16 22:20:36 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,15 +1,10 @@
-# python3
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
     text_input = input()
     if "I" in text_input: 
-#         pattern = input()
-#         text = input()
         return (input().rstrip(), input().rstrip())
-
     if "F" in text_input:
         filename = "06"
         if "a" in filename:
@@ -17,19 +12,13 @@ def read_input():
         else:
             filename = "tests/" + filename
             f = open(filename)
-#             pattern = f.readline()
-#             text = f.readline()
             return (f.readline().rstrip(), f.readline().rstrip())
             f.close()
-           
-    
     # after input type choice
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
-    
     # return both lines in one return
-    
     # this is the sample return, notice the rstrip function
     return (input().rstrip(), input().rstrip())
 
@@ -65,43 +54,7 @@ def get_occurrences(pattern, text):
             current_text_hash = (prime * (current_text_hash - ord(text[i]) * h) + ord(text[i + pattern_length])) % q
 
     return result
-    # and return an iterable variable
-#     return [0]
-
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
-# # python3
-
-# def read_input():
-#     # this function needs to aquire input both from keyboard and file
-#     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-#     # after input type choice
-#     # read two lines 
-#     # first line is pattern 
-#     # second line is text in which to look for pattern 
-    
-#     # return both lines in one return
-    
-#     # this is the sample return, notice the rstrip function
-#     return (input().rstrip(), input().rstrip())
-
-# def print_occurrences(output):
-#     # this function should control output, it doesn't need any return
-#     print(' '.join(map(str, output)))
-
-# def get_occurrences(pattern, text):
-#     # this function should find the occurances using Rabin Karp alghoritm 
-
-#     # and return an iterable variable
-#     return [0]
-
-
-# # this part launches the functions
-# if __name__ == '__main__':
-#     print_occurrences(get_occurrences(*read_input()))
-
